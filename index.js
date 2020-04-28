fed074efa68fcb87d7da26a14492d8176d764b2c
pouyajabbarisani <pouyajabbarisani@gmail.com>
2020-04-28 14:41:52 +0300

    esm config added to file instead of commandline. test database option added

--- index.js
+++ index.js
@@ -1,27 +1,2 @@
-import express from 'express';
-const app = express();
-import cookieParser from 'cookie-parser';
-require('dotenv').config({ path: __dirname + '/.env' });
-app.use(cookieParser(process.env.COOKIE_SIGN));
-import { ApolloServer } from 'apollo-server-express';
-import mongoose from 'mongoose';
-import { PORT } from './config';
-import schemaDirectives from './directives';
-
-// Connect to DB
-import { mongoURI } from './config'
-mongoose.connect(mongoURI, { useNewUrlParser: true }).then(() => console.log('🍃 Database Successfully connected!')).catch(err => console.log(err));
-
-import typeDefs from './typeDefs'
-import resolvers from './resolvers'
-
-const server = new ApolloServer({
-   typeDefs,
-   resolvers,
-   schemaDirectives,
-   playgorund: process.env.NODE_ENV !== 'production',
-   context: ({ req, res }) => ({ req, res })
-});
-
-server.applyMiddleware({ app });
-app.listen({ port: PORT }, () => console.log(`⚙️✅Server is running on http://localhost:${PORT}${server.graphqlPath}`));
+require = require("esm")(module/*, options*/)
+module.exports = require("./main.js")
---
+++ main.js
@@ -0,0 +1,29 @@
+import express from 'express';
+const app = express();
+
+import cookieParser from 'cookie-parser';
+require('dotenv').config({ path: __dirname + '/.env' });
+app.use(cookieParser(process.env.COOKIE_SIGN));
+import { ApolloServer } from 'apollo-server-express';
+import mongoose from 'mongoose';
+import { PORT } from './config';
+import schemaDirectives from './directives';
+
+// Connect to DB
+import config from './config';
+mongoose.connect(process.env.ENV_TYPE == 'test' ? config.mongoURI_TEST : config.mongoURI, { useNewUrlParser: true }).then(() => console.log(`🍃${process.env.ENV_TYPE == 'test' ? 'Test' : ''} Database Successfully connected!`)).catch(err => console.log(err));
+
+
+import typeDefs from './typeDefs'
+import resolvers from './resolvers'
+
+const server = new ApolloServer({
+   typeDefs,
+   resolvers,
+   schemaDirectives,
+   playgorund: process.env.NODE_ENV !== 'production',
+   context: ({ req, res }) => ({ req, res })
+});
+
+server.applyMiddleware({ app });
+app.listen({ port: PORT }, () => console.log(`⚙️✅Server is running on http://localhost:${PORT}${server.graphqlPath}`));
